5e451f896276a80b31e78f5b7321d488bda9044c
raparicio6 <rodrigoaparicio6@gmail.com>
2020-05-01 05:22:50 -0300

    allowing connection without authentication

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { host, port, name, username, password } = require('../config').common.database;
 
-const connectionString = `mongodb://${username}:${password}@${host}:${port}/${name}`;
+const connectionString = username
+  ? `mongodb://${username}:${password}@${host}:${port}/${name}`
+  : `mongodb://${host}:${port}/${name}`;
 module.exports = () =>
   mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
